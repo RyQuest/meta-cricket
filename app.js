ad994ed291484b2daf4131a2c4600d6f872e0ea6
Aman Totla <amantotla@questglt.org>
2022-04-27 14:01:19 +0530

    first commit

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://data123:data123@cluster0.exouw.mongodb.net/Your_World?retryWrites=true&w=majority')
-.then(() => console.log('MongoDB Database Connected'))
-.catch(err => console.log(err))  
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// rajkumar sir private cluster
+mongoose.connect('mongodb+srv://data123:data123@cluster0.exouw.mongodb.net/Meta-Cricket?retryWrites=true&w=majority')
+.then(() => console.log('MongoDB Database Connected'))
+.catch(err => console.log(err)) 
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,44 +45,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-const ipfsAPI = require('ipfs-api');
-const express = require('express');
-const fs = require('fs');
-const app = express();
-
-//Connceting to the ipfs network via infura gateway
-const ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'})
-
-//Reading file from computer
-let testFile = fs.readFileSync("PATH_OF_FILE");
-//Creating buffer for ipfs function to add file to the system
-let testBuffer = new Buffer(testFile);
-
-//Addfile router for adding file a local file to the IPFS network without any local node
-app.get('/addfile', function(req, res) {
-
-    ipfs.files.add(testBuffer, function (err, file) {
-        if (err) {
-          console.log(err);
-        }
-        console.log(file)
-      })
-
-})
-//Getting the uploaded file via hash code.
-app.get('/getfile', function(req, res) {
-    
-    //This hash is returned hash of addFile router.
-    const validCID = 'HASH_CODE'
-
-    ipfs.files.get(validCID, function (err, files) {
-        files.forEach((file) => {
-          console.log(file.path)
-          console.log(file.content.toString('utf8'))
-        })
-      })
-
-})
-app.listen(3000, () => console.log('App listening on port 3000!'))
 
 module.exports = app;
--- services/kycServices.js
+++
@@ -1,9 +0,0 @@
-const {kycInfo}=require('../models/kyc')
-const submit=async(object)=>{
-    const user=new kycInfo(object)
-    await user.save()
-    return user
-}
-module.exports={
-    submit
-}
